c71d5adec797b63475c7b2c13443b587c1d36286
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-05-06 11:05:38 -0300

    components

---
+++ revisao-para-prova/revisao-prova/src/components/Gallery.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Gallery(){
+    return(
+        <section>
+            <div className="gallery-container">
+                <div className="gallery-shelf">
+                    
+
+                </div>
+                <div className="gallery-shelf">
+
+
+                </div>
+                <div className="gallery-shelf">
+
+
+                </div>
+                <div className="gallery-shelf">
+
+
+
+                </div>
+            </div>
+        </section>
+    
+    )
+
+}
