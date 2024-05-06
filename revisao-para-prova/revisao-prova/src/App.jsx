1e50a37aa8f179bb483dce6366b6c16874c265fa
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-05-06 12:04:29 -0300

    commit2

--- revisao-para-prova/revisao-prova/src/App.jsx
+++ revisao-para-prova/revisao-prova/src/App.jsx
@@ -3,7 +3,7 @@ import './style.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Gallery from './components/Gallery'
-
+import Listitems from './components/Listitems'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +12,7 @@ function App() {
   <main>
     <Header />
     <Gallery />
+    <Listitems />
     <Footer />
   </main>
   )
---
+++ revisao-para-prova/revisao-prova/src/components/Listitems.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Listitems(){
+    return(
+        <section>
+            <div className="list-container">
+                <ul className="list"> <h3>Lista de Itens Luquisticos</h3>
+                    <li>Diogo dos Santos Pastor</li>
+                    <li>Ruan</li>
+                    <li>Tasca</li>
+                    <li>Henrique</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
